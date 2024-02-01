f7947920b5548f64851c1180de8cac00f964bb93
Naveen Sai <38736123+naveen675@users.noreply.github.com>
2024-02-01 12:29:16 +0530

    Amazon_customer_review_generator.py

---
+++ GenAIForEcomm/Amazon_customer_review_generator.py
@@ -0,0 +1,2 @@
+## Generates The summarised review based on some data points by feeding all the data for a particular product in Amazon website. 
+## It will give The details of how customer rating the product and which is most liked one and disliked one. along with Summary Comments.
